fa145a714fef0f7dbade7ed4534cf7c05d6bf2d0
Abdullah Osama <xaydenrosario@gmail.com>
2019-05-23 21:35:04 +0200

    Implement user schema and register and login validations

---
+++ components/users/model.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const shortid = require('shortid');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  _id: {
+    type: String,
+    default: shortid.generate
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 255
+  },
+  role: {
+    type: String,
+    enum: ['student', 'teacher', 'assistant']
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+});
+
+
+
+userSchema.methods.hashPassword = async password => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+userSchema.methods.validatePassword = async (passedPassword, password) => {
+  return await bcrypt.compare(passedPassword, password);
+};
+
+userSchema.methods.generateAuthToken = function() {
+  return jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin, role: this.role },
+    'SecureKey'
+  );
+};
+
+exports.userCollection = mongoose.model('User', userSchema);
---
+++ components/users/schema.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+
+function validateRegister(user) {
+  const schema = {
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .required(),
+    role: Joi.string()
+      .valid('student', 'teacher', 'assistant')
+      .required(),
+    password: Joi.string()
+      .min(8)
+      .max(255)
+      .required()
+  };
+  return Joi.validate(user, schema);
+}
+
+function validateLogin(req) {
+  const schema = {
+    email: Joi.string()
+    .email({ minDomainAtoms: 2 })
+    .required(),
+    password: Joi.string()
+    .min(6)
+    .max(255)
+    .required()
+  };
+  return Joi.validate(req, schema);
+}
+
+exports.validateRegister = validateRegister;
+exports.validateLogin = validateLogin;
