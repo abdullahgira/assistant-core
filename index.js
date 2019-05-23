1474fe5207d2fd9c595c21578672148d8f3315e4
Abdullah Osama <xaydenrosario@gmail.com>
2019-05-23 02:23:46 +0200

    Setup local mongoose db

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost:27017/assistant-core', { useNewUrlParser: true })
+  .then(() => logger.info('Connected to MongoDB...'))
+  .catch(err => winston.error('Could not connect to MongoDB...', err));
 
 const bodyParser = require('body-parser');
 const app = express();
