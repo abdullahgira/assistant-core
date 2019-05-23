6d68cc503dbd013ea5d0be89ea13987ec4def8ea
Abdullah Osama <xaydenrosario@gmail.com>
2019-05-23 02:28:32 +0200

    Add and configure winston logger

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const winston = require('winston');
+
+const bodyParser = require('body-parser');
+const app = express();
 
 mongoose
   .connect('mongodb://localhost:27017/assistant-core', { useNewUrlParser: true })
   .then(() => logger.info('Connected to MongoDB...'))
   .catch(err => winston.error('Could not connect to MongoDB...', err));
 
-const bodyParser = require('body-parser');
-const app = express();
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: './logs/info.log' })
+  ]
+});
 
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listining on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Listining on port ${PORT}`));
